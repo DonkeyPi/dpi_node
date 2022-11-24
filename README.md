cdb7713106605c21ab935851a1c509160318cd78
Samuel Ventura <samuel.ventura@yeico.com>
2022-11-24 17:56:33 -0600

    Type renamed to handler.

--- README.md
+++ README.md
@@ -4,18 +4,17 @@ Ash.Node is a tree building DSL with diffing.
 
 ```elixir
 use Ash.Node
-alias UI.Panel
-alias UI.Label
+use Ash.Tui
 node :main, Panel, width: 800, height: 480 do
   node(:label, Label, x: 10, y: 10 text: "Hello")
 end
 ```
 
 Nodes are composed of:
-- ID (any term)
-- Type (any term)
-- Properties (keyword)
-- Children nodes (nested)
+- ID -> any term
+- Handler -> any term, usually a module, function, or just a type tag
+- Properties -> keyword
+- Children nodes -> nested
 
 # Roadmap
 
--- lib/macros.ex
+++ lib/macros.ex
@@ -1,23 +1,24 @@
 defmodule Ash.Node.Macros do
   alias Ash.Node.Parser
 
-  defmacro node(id, type, props) do
+  # Handlers can be modules, functions, or just type tags.
+  defmacro node(id, handler, props) do
     quote do
-      {unquote(id), unquote(type), unquote(props), []}
+      {unquote(id), unquote(handler), unquote(props), []}
     end
   end
 
-  defmacro node(id, module, props, do: inner) do
-    # standard unquoting of a block returns last value
-    # parse to capture each node instance
+  defmacro node(id, handler, props, do: inner) do
+    # Standard unquoting of a block returns last value.
+    # Parse to capture each node instance.
     inner = Parser.parse(inner)
 
-    # flatten allows for nested children generators
-    # filter allow for removal of nil children
+    # Flatten allows for nested children generators.
+    # Filter allow for removal of nil children.
     quote do
       children = unquote(inner) |> List.flatten()
       children = Enum.filter(children, fn child -> child != nil end)
-      {unquote(id), unquote(module), unquote(props), children}
+      {unquote(id), unquote(handler), unquote(props), children}
     end
   end
 end
